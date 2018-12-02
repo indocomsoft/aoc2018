7f35bf68c311e09838dc6429def081233e79eb1e
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-12-03 00:57:46 +0800

    Add attempt in scheme day 2

---
+++ 02/scheme/daytwo.rkt
@@ -0,0 +1,34 @@
+#lang racket
+(define (read-input) (file->lines "../input"))
+
+(define (part-one)
+  (let ([freqs (map letter-freq (read-input))])
+   (* (length (filter (lambda (x) (member 2 x)) freqs))
+      (length (filter (lambda (x) (member 3 x)) freqs)))))
+
+(define (letter-freq str)
+  (map (lambda (x) (length x)) (group-by (lambda (x) x) (string->list str))))
+
+(define (part-two)
+  (let ([data (read-input)])
+    (stream-first-non-null
+      (stream-map (lambda (x) (stream-first-non-null
+                                (stream-map (lambda (y) (find-one-different x y))
+                                            data)))
+                  data))))
+
+(define (find-one-different x y)
+  (let ([common (string-find-common x y)])
+    (if (= (- (string-length x) 1) (string-length common)) common null)))
+
+(define (string-find-common x y)
+  (list->string (map (lambda (x) (car x))
+                     (filter (lambda (x) (equal? (car x) (cdr x)))
+                             (map cons (string->list x) (string->list y))))))
+
+(define (stream-first-non-null str)
+  (let ([filtered (stream-filter (lambda (x) (not (null? x))) str)])
+    (if (stream-empty? filtered) null (stream-first filtered))))
+
+(println (part-one))
+(println (part-two))
